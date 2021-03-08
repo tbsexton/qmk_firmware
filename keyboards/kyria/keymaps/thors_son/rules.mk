028e701c7487e04620517c17b1b78cb8bd2f8364
Thurston Sexton <thurston.sexton@nist.gov>
2021-03-08 17:13:18 -0500

    add rules.mk

---
+++ keyboards/kyria/keymaps/thors_son/rules.mk
@@ -0,0 +1,3 @@
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+LEADER_ENABLE = yes        # Enables the use of the leader key
